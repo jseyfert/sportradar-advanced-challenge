a1f333019d86478736632c701b41372bf083cc84
John Seyfert <johnseyfert@gmail.com>
2022-11-14 00:29:06 -0700

    add players route

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # sportradar-advanced-challenge
 
-1. npx sequelize-cli db:migrate
+To start the project:
+
+1. npm run setup --prefix api
+2. npm start --prefix api
--- api/index.js
+++ api/index.js
@@ -1,5 +1,38 @@
+const { Op } = require("sequelize");
+const models = require("./models/index");
+
 const express = require("express");
 const app = express();
 const port = 4000;
+
+app.get("/players", function (req, res) {
+  const search = req.query.search ? `%${req.query.search}%` : "%%";
+  models.Player.findAll({
+    where: {
+      [Op.or]: [
+        {
+          playerId: {
+            [Op.like]: search,
+          },
+        },
+        {
+          playerName: {
+            [Op.like]: search,
+          },
+        },
+        {
+          teamId: {
+            [Op.like]: search,
+          },
+        },
+        {
+          teamName: {
+            [Op.like]: search,
+          },
+        },
+      ],
+    },
+  }).then((players) => res.json(players));
+});
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
